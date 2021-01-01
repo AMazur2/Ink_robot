539a34cb692c694626d88f88ec5d5bc6bff1a25a
AMazur2 <45732226+AMazur2@users.noreply.github.com>
2021-01-01 19:06:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void saveShelf(Shelf shelf, string outfilename) //cos z tym jest nie tak
+void saveShelf(Shelf shelf, string outfilename) 
 {
     vector<Ink> s = shelf.getShelf();
     int size = s.size();
@@ -17,15 +17,26 @@ void saveShelf(Shelf shelf, string outfilename) //cos z tym jest nie tak
     
     ofstream outfile;
     outfile.open(outfilename);
-    outfile << data << endl;                    //do pliku wpisywane sa dziwne znaki na koncu 
+    outfile << data << endl;                    
     outfile.close();
 }
 
-void solve(Shelf shelf, string outFileName)
+void solve(Shelf shelf, string outFileName, string solver)
 {
     shelf.showShelf();
     Robot r;
-    r.brutalSolver(&shelf);
+    if(solver == "brutal")
+        r.brutalSolver(&shelf);
+    else if (solver == "naive")
+    {
+        cout << "TODO: naive solver" << endl;
+        return;
+    }
+    else
+    {
+        cout << "Unknown mode" << endl;
+        return; 
+    }
     shelf.showShelf();
     saveShelf(shelf, outFileName);
 }
@@ -40,39 +51,42 @@ int main(int argc, char *argv[])
 
         if(mode == "-m1")
         {
-            if(argc > 3)
+            if(argc > 4)
             {
-                Shelf shelf = Shelf(string(argv[2]));
-                solve(shelf, string(argv[3]));
+                string solver = argv[2];
+                Shelf shelf = Shelf(string(argv[3]));
+                solve(shelf, string(argv[4]), solver);
             }
             else 
             {
-                cout << "Error: No filename given!" << endl;
+                cout << "Error: Some parameter(s) missing!" << endl;
                 cout << "For help type 'h' or 'help'" << endl;
                 return 1;
             }
         }
         else if (mode == "-m2")
         {
-            if(argc > 2)
+
+            if(argc > 3)
             {
+                string solver = argv[2];
                 if(argc==3)
                 {
-                    Shelf shelf = Shelf(stoi(argv[2]));
-                    solve(shelf, outFile);
+                    Shelf shelf = Shelf(stoi(argv[3]));
+                    solve(shelf, outFile, solver);
                 }
-                else if (argc%2 == 1)
+                else if (argc%2 == 0)
                 {
-                    int amount = stoi(argv[2]);
-                    int number = (argc-3)/2;
+                    int amount = stoi(argv[3]);
+                    int number = (argc-4)/2;
                     int sum = 0;
                     string seq[number];
                     int possibilities[number];
                 
                     for(int i = 0; i < number; i++)
                     {
-                        seq[i] = argv[2*i+3];
-                        possibilities[i] = stoi(argv[2*i+4]);
+                        seq[i] = argv[2*i+4];
+                        possibilities[i] = stoi(argv[2*i+5]);
                         sum += possibilities[i];
                         if(sum > 100)
                         {
@@ -82,7 +96,7 @@ int main(int argc, char *argv[])
                     }
 
                     Shelf shelf = Shelf(amount, seq, possibilities, number);
-                    solve(shelf, outFile);
+                    solve(shelf, outFile, solver);
                 }
                 else
                 {
@@ -103,17 +117,18 @@ int main(int argc, char *argv[])
         {
             if(argc == 6)
             {
-                int amount = stoi(argv[2]);
-                int step = stoi(argv[3]);
-                int stepNum = stoi(argv[4]);
-                int instancesNum = stoi(argv[5]);
+                string solver = argv[2];
+                int amount = stoi(argv[3]);
+                int step = stoi(argv[4]);
+                int stepNum = stoi(argv[5]);
+                int instancesNum = stoi(argv[6]);
 
                 for(int i = 0; i < stepNum; i++)
                 {
                     for(int j = 0; j < instancesNum; ++j)
                     {
                         Shelf shelf = Shelf(amount);
-                        solve(shelf, outFile);
+                        solve(shelf, outFile, solver);
                     }
                     amount += step;
                 }
@@ -130,18 +145,20 @@ int main(int argc, char *argv[])
         {
             cout << endl;
             cout << "\tMode 1 - from file" << endl << endl;
-            cout << "prog -m1 inFileName outFileName" << endl << endl << endl;
+            cout << "prog -m1 solverName inFileName outFileName" << endl << endl << endl;
             cout << "\tMode 2 - generator" << endl;
             cout << "For creating a random shelf containing 'number' elements, type:" << endl << endl;
-            cout << "prog -m2 number" << endl << endl << endl;
+            cout << "prog -m2 solverName number " << endl << endl << endl;
             cout << "If you want to give sequences with probabilities use syntax:" << endl << endl;
-            cout << "prgramName -m2 number seq1 prob1 seq2 prob2 ...." << endl << endl;
+            cout << "prog -m2 solverName number seq1 prob1 seq2 prob2 ...." << endl << endl;
             cout << "number stands for amount of random drawing from these sequences and single containers" << endl;
             cout << "remember that sum of probabilities cannot be greater than 100" << endl << endl << endl;
             cout << "\tMode 3 - multiple problem" << endl << endl;
-            cout << "prog -m3 start step numberOfSteps numberOfInstances" << endl << endl;
+            cout << "prog -m3 solverName start step numberOfSteps numberOfInstances" << endl << endl;
             cout << "numberOfSteps - how many different sizes of problems will be solved" << endl;
-            cout << "numberOfInstances - how many problems of one size will be solved" << endl;
+            cout << "numberOfInstances - how many problems of one size will be solved" << endl << endl;
+            cout << "Posiible solvers:" << endl;
+            cout << "brutal" << "\tnaive" << endl;
         }
         else
         {
