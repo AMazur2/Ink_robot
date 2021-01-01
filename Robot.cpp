49ab285d1a3006dad61c36218861874730d7c74f
AMazur2 <45732226+AMazur2@users.noreply.github.com>
2021-01-01 18:21:04 +0100

    Update Robot.cpp
    Now brutal solver works, added comments

--- Robot.cpp
+++ Robot.cpp
@@ -1,7 +1,7 @@
 #include "Robot.hpp"
 
 
-std::vector<Ink> Robot::moveRight(std::vector<Ink> shelf, int start)
+std::vector<Ink> Robot::moveRight(std::vector<Ink> shelf, int start)    //przesuwa w prawo 4 pojemniki zaczynajac od indeksu 'start' a reszte dosuwa zapelniajac puste pole
 {
     Ink temp[4];
     int shelfSize = shelf.size();
@@ -17,23 +17,24 @@ std::vector<Ink> Robot::moveRight(std::vector<Ink> shelf, int start)
     return shelf;
 }
 
-std::vector<Ink> Robot::moveLeft(std::vector<Ink> shelf, int start, int stop)
-{
-
+std::vector<Ink> Robot::moveLeft(std::vector<Ink> shelf, int start, int stop)   //przesuwa pojemniki tak, aby pojemnik na miejscu 'stop+1' znalazl sie na miejscu 'start'
+{                                                                       //potencjalne TODO: upewnic sie ze miedzy start a stop jest 4k pojemnikow, chociaz gleboko wierze
+                                                                        //ze wzory ktore wyprowadzilem, zapewniaja juz optymalna ilosc pojemnikow do przesuwania
     int toMove = (stop - start + 1)/4;
+
     for(int i = 0; i < toMove; i++)
     {
         Ink temporary[4];
         for(int j = 0; j < 4; ++j)
             temporary[j] = shelf[start+j];
     
-        int left = shelf.size() - stop - 1;
+        int left = shelf.size() - start - 4;
 
         for(int j = 0; j < left; ++j)
             shelf[start+j] = shelf[start+j+4];
         
         for(int j = 0; j < 4; ++j)
-            shelf[stop+1+j] = temporary[j];
+            shelf[shelf.size()-4+j] = temporary[j];
     }
     return shelf;
 }
@@ -41,64 +42,65 @@ std::vector<Ink> Robot::moveLeft(std::vector<Ink> shelf, int start, int stop)
 std::vector<Ink> Robot::brutalForce(std::vector<Ink> shelf, int start, int nextColour)
 {
     std::vector<Ink> temporaryShelf;
-    if(shelf[start].getColour() == types[nextColour])
+    if(shelf[start].getColour() == types[nextColour])                       // od razu mamy odpowiedni pojemnik na miejscu
     {
-        if(start+1 < shelf.size())
+        if(start+1 < shelf.size())                                          //jezeli istnieje nastepny pojemnik to rekursywnie przechodzimy dalej
             temporaryShelf = brutalForce(shelf, start+1, (nextColour+1)%4);
-        else
+        else                                                                //jezeli nie to zwracamy obecną półkę
             return shelf;
     }
     else
     {
-        if(shelf.size()-start > 3)
-        {
+        if(shelf.size()-start > 4)                                          //pozostala ilosc pojemnikow musi byc wieksza od 4
+        {                                                                   //aby mozna bylo dokonac jakichkolwiek zmian w ciagu
             bool notFound = true;
-            int index = start;
-            while(notFound)                     //znajdujemy najblizszy pojemnik ktory powinien byc nastepny
+            int index = start;                                              //miejsce najblizszego pojemnika ktory powinien byc nastepny
+            while(notFound)                                                 //znajdujemy najblizszy pojemnik ktory powinien byc nastepny
             {
-                index++;
                 if(index < shelf.size())
                 {
                     if(shelf[index].getColour() == types[nextColour])
                         notFound = false;
+                    else
+                        index++;
                 }
-                else
+                else                                                        //nie znalezlismy - zwracamy półkę i koniec algorytmu
                     return shelf;
                 
             }
 
-            std::vector<Ink> temp;
-            if((index)%4 == nextColour)
-                temporaryShelf = brutalForce(moveLeft(shelf, start, index-1), start+1, (nextColour+1)%4);
+            std::vector<Ink> temp;                                          //pomocniczy wektor
+            if((index)%4 == nextColour)                                     //jezeli pojemnik jest na poprawnym miejscu, majac pod uwaga miejsca w czworkach
+                temporaryShelf = brutalForce(moveLeft(shelf, start, index-1), start+1, (nextColour+1)%4);   //przesuwamy wszystkie czworki miedzy obecnym poczatkiem a pojemnikiem
             else
             {
-                int position = 0;
+                int position = 0;                                           //miejsce w przenoszonej czworce
                 bool notFound = true;
-                while(notFound)
-                {
+                while(notFound)                                             //sprawdzamy na jakiej pozycji (majac na uwadze ostatnie 4 miejsca) powinien byc pojemnik
+                {                                                           //aby mozna bylo poprawnie go przeniesc
                     if((shelf.size()-4+position)%4 == nextColour)
                         notFound = false;
                     else
                         position++;
                 }
 
-                if(index > shelf.size()-4+position)
-                {
+                if(index > shelf.size()-4+position)                         //jezeli nie mozemy zrobic takiej czworki aby dany pojemnik byl na odpowiednim miejscu
+                {                                                           //przenosimy poprzedzajaca czworke (przypadek ze pojemnik jest za daleko na prawo aby go przeniesc)
                     temporaryShelf = brutalForce(moveRight(shelf, index-4), start, nextColour);
                 }
                 else
                 {
-                    if(index-position >= start)
-                    {
-                        std::vector<Ink> temp = moveLeft(moveRight(shelf, index-position), start, shelf.size()-5+position);
-                        temporaryShelf = brutalForce(temp, start+1, (nextColour+1)%4);
+                    if(index-position >= start)                             //jezeli nasz pojemnik jest odpowiednio oddalony od punktu startu (mozemy stworzyc czworke
+                    {                                                       //w ktorej pojemnik bedzie na odpowiednim miejscu)
+                        std::vector<Ink> temp = moveLeft(moveRight(shelf, index-position), start, shelf.size()-5+position); //przenosimy ta czworke na prawo a potem przesuwamy
+                        temporaryShelf = brutalForce(temp, start+1, (nextColour+1)%4);      //tak aby nasz pojemnik byl na miejscu 'start' i kontynuujemy algorytm
                     }
-                    else
-                    {
-                        int newPosition = shelf.size()-index+start;
-                        std::vector<Ink> temp = moveRight(moveRight(shelf, start), newPosition-position); 
-                        temporaryShelf = brutalForce(moveLeft(temp, start, shelf.size()-5+position), start+1, (nextColour+1)%4);
-                    }   
+                    else                                                    //pojemnik jest zbyt blisko startu wiec musimy go oddalic
+                    {                                                       //przenosimy czworke zaczynajac od pojemnika znajdujacego sie na pozycji 'start'
+                        int newPosition = shelf.size()-4+index-start;       //obliczamy nowa pozycje naszego pojemnika i wtedy gdy mamy juz na pewno wystarczajaca ilosc
+                        std::vector<Ink> temp = moveRight(moveRight(shelf, start), newPosition-position);   //pojemnikow przenoismy nasz pojemnik aby znajdowal sie na odpowiedniej pozycji
+                        temporaryShelf = brutalForce(moveLeft(temp, start, shelf.size()-5+position), start+1, (nextColour+1)%4);    //na koniec doprowadzamy nasz pojemnik
+                    }                                                       //na pozycje 'start' i kontynuujemy algorytm
                 }
                 
             }
