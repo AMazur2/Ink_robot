e424b6fb13eec949bb0381bfd9bb99f7b67ac5ed
AMazur2 <45732226+AMazur2@users.noreply.github.com>
2021-01-02 22:10:32 +0100

    Update Robot.hpp

--- Robot.hpp
+++ Robot.hpp
@@ -3,17 +3,27 @@
 
 #include "Shelf.hpp"
 
+
 class Robot
 {
     private:
         char types[4] = {'C', 'M', 'Y', 'K'};
+        int findNumber(std::vector<Ink> shelf);
+        std::vector<int> findFours(std::vector<Ink> shelf);
         std::vector<Ink> moveRight(std::vector<Ink> shelf, int start);
         std::vector<Ink> moveLeft(std::vector<Ink> shelf, int start, int stop);
+        std::vector<Ink> sortFours(std::vector<Ink> shelf, std::vector<int> fours, int start);
         std::vector<Ink> brutalForce(std::vector<Ink> shelf, int start, int nextColour);
+        std::vector<Ink> naive(std::vector<Ink> shelf, int start, int nextColour);
+        std::vector<Ink> maximalSubstring(std::vector<Ink> shelf, int start, int nextColour);
+        std::vector<Ink> positions(std::vector<Ink> shelf, int start, int nextColour, int toSort);
     public:
         Robot() {};
         ~Robot() {};
         void brutalSolver(Shelf *shelf);
+        void naiveSolver(Shelf *shelf);
+        void maximalSubstringSolver(Shelf *shelf);   //najwieksze podciagi
+        void positionSolver(Shelf *shelf);  //optymalizacja czworkowa
 };
 
 #endif
